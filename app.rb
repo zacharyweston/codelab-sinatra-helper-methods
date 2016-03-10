420f5806fc02533cdb491dbf840278a1769ed0fe
Zak Weston <zweston@MHC.local>
2016-03-10 11:10:04 -0500

    Fixed issues

--- app.rb
+++ app.rb
@@ -1,16 +1,12 @@
 require 'sinatra'
-
+# Top-level class documentation comment
 class ZakWebApp < Sinatra::Base
   get '/' do
     erb :home
   end
-
   helpers do
-
-    def icon_button(icon, type, message)
-      # type default value 'default'; message default value ''
+    def icon_button(icon, type = 'default', message = '')
       %(
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <button type="#{type}" class="btn btn-default btn-lg">
           <span class="#{icon}" aria-hidden="true"></span> #{message}
         </button>
@@ -24,15 +20,15 @@ class ZakWebApp < Sinatra::Base
     def pluralize(singular_name, count)
       if count == 1
         %(
-            <h1>
-              #{count} #{singular_name}
-            </h1>
+          <h1>
+            #{count} #{singular_name}
+          </h1>
         )
       else
         %(
-            <h1>
-              #{count} #{singular_name}'s
-            </h1>
+          <h1>
+            #{count} #{singular_name}'s
+          </h1>
         )
       end
     end
@@ -40,15 +36,13 @@ class ZakWebApp < Sinatra::Base
     def use_google_font(font)
       # fontName = font.gsub!(/\s/,'+')
       %(
-        <link href='https://fonts.googleapis.com/css?family=#{font}' rel='stylesheet' type='text/css'>
         <style>
           p {
-          font-family: #{font}, sans-serif;
+            font-family: #{font}, sans-serif;
           }
         </style>
         <p>#{font}</p>
       )
     end
-
   end
 end
